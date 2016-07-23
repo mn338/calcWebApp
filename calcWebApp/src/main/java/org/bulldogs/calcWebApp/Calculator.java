84ff73ec3d1f7c1d67988ffd76955a58d67c59d1
Nadeem <mn338@msstate.edu>
2016-07-23 00:50:12 -0500

    First Commit

---
+++ calcWebApp/src/main/java/org/bulldogs/calcWebApp/Calculator.java
@@ -0,0 +1,28 @@
+package org.bulldogs.calcWebApp;
+
+public class Calculator 
+{
+	public int sub(int a, int b)
+	{
+		return a-b;
+	}
+	
+	public int add(int a, int b)
+	{
+		return a+b;
+	}
+	
+	public int mul(int a, int b)
+	{
+		return a*b;
+	}	
+
+	public int factorial(int a)
+	{		
+		if (a == 1)
+			return 1;
+		
+		else 
+		return a * factorial (a-1);
+	}
+}
